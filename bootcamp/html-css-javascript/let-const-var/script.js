236da0822fc01e6cce133737afe2ca384c286421
AbdurRahman G <abdurrahmangofficial@gmail.com>
2022-11-08 11:28:04 +0530

    JavaScript

---
+++ bootcamp/html-css-javascript/let-const-var/script.js
@@ -0,0 +1,6 @@
+let username = "AbdurRahman G";
+let age = 24;
+let isHappy = true;
+
+let userIntro = "Assalamu Alaikum, My name is " + username + " and I am " + age + " years old and I am happy " + isHappy;
+
