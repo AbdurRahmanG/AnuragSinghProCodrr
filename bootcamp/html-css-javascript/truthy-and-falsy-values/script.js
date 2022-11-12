47bc60298db34de4d1dab8f92c38328922d3f0db
AbdurRahman G <abdurrahmangofficial@gmail.com>
2022-11-12 08:43:14 +0530

    Truthy and Falsy Values in JavaScript

---
+++ bootcamp/html-css-javascript/truthy-and-falsy-values/script.js
@@ -0,0 +1,8 @@
+document.write(`false ${Boolean(false)} `);
+document.write(`0 ${Boolean(0)} `);
+document.write(`-0 ${Boolean(-0)} `);
+document.write(`0n ${Boolean(0n)} `);
+document.write(`"" ${Boolean("")} `);
+document.write(`null ${Boolean(null)} `);
+document.write(`undefined ${Boolean(undefined)} `);
+document.write(`NaN ${Boolean(NaN)} `);
