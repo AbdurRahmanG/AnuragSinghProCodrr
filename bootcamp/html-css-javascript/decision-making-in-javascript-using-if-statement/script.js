57523123d03f026180ef26a19cbbd3a59b72d0f2
AbdurRahman G <abdurrahmangofficial@gmail.com>
2022-11-16 10:41:52 +0530

    Optimize Decision Making Using Else If and Else

--- bootcamp/html-css-javascript/decision-making-in-javascript-using-if-statement/script.js
+++ bootcamp/html-css-javascript/decision-making-in-javascript-using-if-statement/script.js
@@ -1,19 +1,39 @@
-const username = prompt("Type your Full Name");
-const userAge = prompt("Type your Age");
+// const username = prompt("Type your Full Name") || "AbdurRahman G";
+// const userAge = prompt("Type your Age") || 24;
 
-if(username === null || username === "" || username === " ") {
-    document.write(`Name: AbdurRahman G. `);
-} else {
+// Or
+
+// const username = prompt("Type your Full Name");
+// const userAge = prompt("Type your Age");
+
+// Or
+const username = prompt("Type your Full Name");
+const userAge = parseInt(prompt("Type your Age"));
+
+// Truthy or Falsy
+if(username){
     document.write(`Name: ${username}. `);
 }
 
-if(userAge === null || userAge == "" || userAge == " ") {
-    document.write(`Age: 24. `);
-} else {
-    document.write(`Age: ${parseInt(userAge)}. `);
+if(userAge || 0){
+    document.write(`Age: ${userAge}. `);
 }
 
-userAge = parseInt(userAge);
+// End of Truthy or Falsy
+
+// if(username === null || username === "" || username === " ") {
+//     document.write(`Name: AbdurRahman G. `);
+// } else {
+//     document.write(`Name: ${username}. `);
+// }
+
+// if(userAge === null || userAge == "" || userAge == " ") {
+//     document.write(`Age: 24. `);
+// } else {
+//     document.write(`Age: ${parseInt(userAge)}. `);
+// }
+
+// userAge = parseInt(userAge);
 
 if(userAge >= 18 && userAge <= 24) {
     document.write("You are a college student and you are studying computer science. ");
@@ -22,6 +42,8 @@ if(userAge >= 18 && userAge <= 24) {
 } else if(userAge > 45) {
     document.write("You are old person. ");
 }
+
+document.write("Kindly enter a valid name and age!")
 
 
 
