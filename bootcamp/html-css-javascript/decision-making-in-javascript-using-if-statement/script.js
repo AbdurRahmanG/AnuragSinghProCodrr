430eb7dd45a9d38b8fa4884ff0c66b953843041c
AbdurRahman G <abdurrahmangofficial@gmail.com>
2022-11-18 20:18:53 +0530

    Switch case statement

--- bootcamp/html-css-javascript/decision-making-in-javascript-using-if-statement/script.js
+++ bootcamp/html-css-javascript/decision-making-in-javascript-using-if-statement/script.js
@@ -1,3 +1,7 @@
+
+// =================
+// If else 
+
 // const username = prompt("Type your Full Name") || "AbdurRahman G";
 // const userAge = prompt("Type your Age") || 24;
 
@@ -7,17 +11,17 @@
 // const userAge = prompt("Type your Age");
 
 // Or
-const username = prompt("Type your Full Name");
-const userAge = parseInt(prompt("Type your Age"));
+// const username = prompt("Type your Full Name");
+// const userAge = parseInt(prompt("Type your Age"));
 
 // Truthy or Falsy
-if(username){
-    document.write(`Name: ${username}. `);
-}
+// if(username){
+//     document.write(`Name: ${username}. `);
+// }
 
-if(userAge || 0){
-    document.write(`Age: ${userAge}. `);
-}
+// if(userAge || 0){
+//     document.write(`Age: ${userAge}. `);
+// }
 
 // End of Truthy or Falsy
 
@@ -35,15 +39,71 @@ if(userAge || 0){
 
 // userAge = parseInt(userAge);
 
-if(userAge >= 18 && userAge <= 24) {
-    document.write("You are a college student and you are studying computer science. ");
-} else if(userAge >= 25 && userAge <= 45){
-    document.write("You are a working professional. ");
-} else if(userAge > 45) {
-    document.write("You are old person. ");
+// if(userAge >= 18 && userAge <= 24) {
+//     document.write("You are a college student and you are studying computer science. ");
+// } else if(userAge >= 25 && userAge <= 45){
+//     document.write("You are a working professional. ");
+// } else if(userAge > 45) {
+//     document.write("You are old person. ");
+// }
+
+// document.write("Kindly enter a valid name and age!")
+
+// ==================
+// Switch case
+
+// debugger
+
+// const dayNumber = parseInt(prompt("Enter Day in Number Like (0,1,2,3,4,5,6)"));
+
+// switch (dayNumber) {
+//     case 0:
+//         console.log("It's Thursday Today.");
+//         document.write("It's Thursday Today.");
+//         break;
+//     case 1:
+//         console.log("It's Friday Today.");  
+//         document.write("It's Friday Today.");
+//         break;
+//     case 2:
+//         console.log("It's Saturday Today.");
+//         document.write("It's Saturday Today.");
+//         break;
+//     case 3:
+//         console.log("It's Sunday Today.");
+//         document.write("It's Sunday Today.");
+//         break;
+//     case 4:
+//         console.log("It's Monday Today.");
+//         document.write("It's Monday Today.");
+//         break;
+//     case 5:
+//         console.log("It's Tuesday Today.");
+//         document.write("It's Tuesday Today.");
+//         break;
+//     case 6:
+//         console.log("It's Wednesday Today.");
+//         document.write("It's Wednesday Today.");
+//         break;
+//     default:
+//         console.log("Kindly Enter A Valid Day Number...");
+//         document.write("Kindly Enter A Valid Day Number...");
+// }
+
+
+// Switch case example 2
+const grade = prompt("Enter your grade in character (A, B, C, D etc.)").toLowerCase();
+
+switch (grade) {
+    case "a":
+        console.log("Congratulations! You did well and You passed the Test.");
+        break;
+    case "b":
+        console.log("You did good, Needs more improvement.");
+        break;
+    case "c":
+        console.log("You did average, But needs more and more improvement.");
+        break;
+    default:
+        console.log("You are failed, you need to be clear with your goal.");
 }
-
-document.write("Kindly enter a valid name and age!")
-
-
-
